442502c41996f39eb8fac878d3a12421f1a20914
Quentin Voiret <quentin.voiret@etu.utc.fr>
2024-04-29 18:28:46 +0200

    Intégration de game.go dans app-base

--- app-base/base.go
+++ app-base/base.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 	"utils"
@@ -26,7 +31,7 @@ func envoyerPixel(positionX int, positionY int, rouge int, vert int, bleu int) {
 }
 
 // Quand le programme n'est pas en train d'écrire, il lit
-func lecture() {
+func lecture(game *utils.Game) {
 	var rcvmsg string
 
 	for {
@@ -36,15 +41,23 @@ func lecture() {
 			utils.DisplayError("app-de-base", "lecture", "Réception de : "+rcvmsg[1:])
 			mutex.Lock()
 			messagePixel := utils.StringToMessagePixel(rcvmsg[1:])
-			changerPixel(messagePixel)
+			utils.DisplayError(monNom, "changerPixel", "Et là bim on change le pixel")
+			messageString := utils.MessagePixelToString(messagePixel)
+			cr, _ := strconv.Atoi(utils.TrouverValeur(messageString, "R"))
+			cb, _ := strconv.Atoi(utils.TrouverValeur(messageString, "B"))
+			cg, _ := strconv.Atoi(utils.TrouverValeur(messageString, "G"))
+			x, _ := strconv.Atoi(utils.TrouverValeur(messageString, "positionX"))
+			y, _ := strconv.Atoi(utils.TrouverValeur(messageString, "positionY"))
+			game.UpdateMatrix(x, y, uint8(cr), uint8(cg), uint8(cb))
 			mutex.Unlock()
+			envoyerPixel(x, y, cr, cg, cb)
 		}
 		rcvmsg = ""
 	}
 }
 
-func changerPixel(messagePixel utils.MessagePixel) {
-	utils.DisplayError(monNom, "changerPixel", "Et là bim on change le pixel")
+func changerPixel(messagePixel utils.MessagePixel, game utils.Game) {
+
 }
 
 var mutex = &sync.Mutex{}
@@ -56,9 +69,69 @@ func main() {
 	flag.Parse()
 	monNom = *pNom + "-" + strconv.Itoa(os.Getpid())
 
+	matrix := make([][]utils.Pixel, 100)
+	for y := 0; y < 100; y++ {
+		matrix[y] = make([]utils.Pixel, 100)
+		for x := 0; x < 100; x++ {
+			matrix[y][x] = utils.Pixel{
+				R: 255,
+				G: 255,
+				B: 255,
+			}
+		}
+	}
+
+	colorWheel, _, err := ebitenutil.NewImageFromFile("color_wheel.png")
+	if err != nil {
+		panic(err)
+	}
+
+	game := &utils.Game{
+		Matrix:        matrix,
+		ColorWheel:    colorWheel,
+		SelectedColor: color.RGBA{R: 0, G: 0, B: 0, A: 0xFF},
+	}
+
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			parts := strings.Split(line, " ")
+			if len(parts) == 5 {
+				x, err := strconv.Atoi(parts[0])
+				if err != nil {
+					continue
+				}
+				y, err := strconv.Atoi(parts[1])
+				if err != nil {
+					continue
+				}
+				cr, err := strconv.Atoi(parts[2])
+				if err != nil {
+					continue
+				}
+				cg, err := strconv.Atoi(parts[3])
+				if err != nil {
+					continue
+				}
+				cb, err := strconv.Atoi(parts[4])
+				if err != nil {
+					continue
+				}
+				game.UpdateMatrix(x, y, uint8(cr), uint8(cg), uint8(cb))
+				fmt.Printf("Updated pixel at (%d, %d) to (%d, %d, %d)\n", x, y, cr, cg, cb)
+			}
+		}
+	}()
+
+	ebiten.RunGame(game)
+
+	ebiten.SetWindowSize(800, 600)
+	ebiten.SetWindowTitle("Pixel-War")
+
 	//Création de 2 go routines qui s'exécutent en parallèle
 	go sendperiodic()
-	go lecture()
+	go lecture(game)
 	//On décide de bloquer le programme principal
 	for {
 		time.Sleep(time.Duration(60) * time.Second)
---
+++ utils/frontend.go
@@ -0,0 +1,78 @@
+package utils
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+)
+
+type Pixel struct {
+	R, G, B uint8
+}
+
+func createImageFromMatrix(matrix [][]Pixel) *ebiten.Image {
+	width := 50
+	height := 50
+	img := ebiten.NewImage(width, height)
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			pixel := matrix[y][x]
+			img.Set(x, y, color.RGBA{R: pixel.R, G: pixel.G, B: pixel.B, A: 0xFF})
+		}
+	}
+
+	return img
+}
+
+type Game struct {
+	Matrix        [][]Pixel
+	ColorWheel    *ebiten.Image
+	SelectedColor color.RGBA
+}
+
+func (g *Game) UpdateMatrix(x int, y int, CR uint8, CG uint8, CB uint8) {
+	g.Matrix[x][y] = Pixel{
+		R: CR,
+		G: CG,
+		B: CB,
+	}
+}
+
+func (g *Game) Update() error {
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		g.UpdateMatrix(y, x, g.SelectedColor.R, g.SelectedColor.G, g.SelectedColor.B)
+		// Oui je sais c'est bizarre mais les coordonnées de la souris ne sont pas comme est ordonnée la matrice
+	}
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+		x, y := ebiten.CursorPosition()
+		x_pourc := x * 100 / 50
+		y_pourc := y * 100 / 50
+
+		R, G, B, _ := g.ColorWheel.At(x_pourc-100, y_pourc).RGBA()
+		g.SelectedColor = color.RGBA{uint8(R), uint8(G), uint8(B), 0xFF}
+
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Draw the main image
+	screen.Fill(color.White)
+	op := &ebiten.DrawImageOptions{}
+	// Adjust position based on desired layout (explained later)
+	op.GeoM.Translate(0, 0)
+
+	img := createImageFromMatrix(g.Matrix)
+	screen.DrawImage(img, op)
+
+	// Draw the color wheel
+	colorWheelOp := &ebiten.DrawImageOptions{}
+	colorWheelOp.GeoM.Translate(100, 0)
+	colorWheelOp.GeoM.Scale(0.5, 0.5)
+	screen.DrawImage(g.ColorWheel, colorWheelOp)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return len(g.Matrix[0]), len(g.Matrix)
+}
