a6192ce5e09fa2b77f3c5f40a67b7e08ac0b5eb2
Dev <marius.bureau@etu.utc.fr>
2024-05-13 23:25:03 +0200

    Changes by voire

--- app-base/frontend.go
+++ app-base/frontend.go
@@ -49,7 +49,7 @@ func (g *Game) Update() error {
 		x, y := ebiten.CursorPosition()
 		if x >= 0 && x < screenWidth && y >= 0 && y < screenHeight {
 			clicGaucheMatrice(false, g, y, x, int(g.SelectedColor.R), int(g.SelectedColor.G), int(g.SelectedColor.B))
-			// Les coordonnées de la souris ne sont pas comme est ordonnée la matrice
+			// Les coordonnées de la souris ne sont pas comme est ordonnée la matrice, donc inversion de x et de y
 		}
 	}
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
